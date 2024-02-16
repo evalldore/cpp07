ff1a6fb70bdbf395e2fce03714cf8ac85a19030a
evalldore <evalldore@gmail.com>
2024-02-15 20:44:59 -0500

    a simple tester

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -2,16 +2,50 @@
 #include <string>
 #include "Array.hpp"
 
-#define MAX_VAL 750
-int main(int, char**) {
-    Array<std::string> numbers(4);
+static void strTest(void) {
+	std::cout << "Strings test." << std::endl;
+	Array<std::string> strArrays(4);
 
-    numbers[3] = "lol";
+	strArrays[3] = "lol";
+	try {
+		strArrays[2] = "poor";
+	} catch(std::exception &e) {
+		std::cout << e.what() << std::endl;
+	};
 
-    try {
-        numbers[4];
-    } catch(std::exception &e) {
-        std::cout << e.what() << std::endl;
-    };
-    std::cout << numbers << std::endl;
+	Array<std::string> copy(strArrays);
+	std::cout << "base: " << &strArrays << ' ' << strArrays << std::endl;
+	std::cout << "copy: " << &copy << ' ' << strArrays << std::endl;
+}
+
+static void intTest(void) {
+	std::cout << "Integer test." << std::endl;
+	Array<int> intArrays(4);
+
+	intArrays[0] = 1;
+	intArrays[1] = 2;
+	intArrays[2] = 3;
+	intArrays[3] = 4;
+
+	Array<int> copy(intArrays);
+	std::cout << "base: " << &intArrays << ' ' << intArrays << std::endl;
+	std::cout << "copy: " << &copy << ' ' << intArrays << std::endl;
+}
+
+static void exceptionsTest(void) {
+	std::cout << "Exceptions test." << std::endl;
+	Array<char> charArrays(10);
+
+	try {
+		charArrays[-1] = 'a';
+	} catch (std::exception &e) {
+		std::cout << e.what() << std::endl;
+	}
+	std::cout << charArrays << std::endl;
+}
+
+int main(void) {
+	strTest();
+	intTest();
+	exceptionsTest();
 }
